ba2e1f8d5610263d175646f079ea230a1bbd5437
zeroliu <zerolxy@gmail.com>
2014-10-19 20:11:42 -0700

    [code3] Started up implementing renderer structure

---
+++ app/scripts/renderer.js
@@ -0,0 +1,20 @@
+define('Renderer', ['jquery'], function($) {
+    'use strict';
+
+    function Renderer() {
+        this.gridContainer = $('.tile-container');
+    }
+
+    Renderer.prototype.draw = function(grid) {
+        var renderer = this;
+        window.requestAnimationFrame(function() {
+            renderer.gridContainer.empty();
+            grid.eachSquared(function(x, y, squared) {
+                console.log(x + ', ' + y);
+                console.log(squared);
+            });
+        });
+    };
+
+    return Renderer;
+});
